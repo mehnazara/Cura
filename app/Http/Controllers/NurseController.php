5e31fc088236ca49a3e18f2da607f9a6e23e6aab
Junu Razeen <junaid.abrar.razeen@gmail.com>
2023-11-14 01:30:56 +0600

    implemented rating and reviews of the nurse profiles

---
+++ app/Http/Controllers/NurseController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Nurse;
+use App\Models\Review;
+
+class NurseController extends Controller
+{
+    // Other methods...
+
+    public function show($id)
+    {
+        $nurse = Nurse::find($id);
+        $reviews = Review::where('nurse_id', $id)->get();
+
+        return view('nurses.show', compact('nurse', 'reviews'));
+    }
+
+    public function storeReview(Request $request, $nurseId)
+    {
+        $request->validate([
+            'rating' => 'required|integer|between:1,5',
+            'comment' => 'nullable|string',
+        ]);
+
+        $review = new Review([
+            'user_id' => auth()->id(),
+            'nurse_id' => $nurseId,
+            'rating' => $request->rating,
+            'comment' => $request->comment,
+        ]);
+
+        $review->save();
+
+        return redirect()->route('nurses.show', $nurseId)->with('success', 'Review submitted successfully!');
+    }
+}
